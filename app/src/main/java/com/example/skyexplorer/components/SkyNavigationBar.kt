e2519bd07305e0787c258ea86581a18f4cd9fdbb
marek <marekgagaczowski@gmail.com>
2025-12-12 13:18:47 +0100

    feat: Update app icon and navigation bar style
    This commit updates the application icon and replaces the default home icon in the navigation bar with a new, custom-designed asset.

--- app/src/main/java/com/example/skyexplorer/components/SkyNavigationBar.kt
+++ app/src/main/java/com/example/skyexplorer/components/SkyNavigationBar.kt
@@ -1,7 +1,9 @@
 package com.example.skyexplorer.components
 
 import android.graphics.drawable.Icon
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.CameraAlt
 import androidx.compose.material.icons.filled.Home
@@ -11,9 +13,17 @@ import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarDefaults
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import com.example.skyexplorer.R
 
 @Composable
 fun AppNavigationBar(
@@ -46,9 +56,11 @@ fun AppNavigationBar(
             },
             icon = {
                 Icon(
-                    imageVector = Icons.Filled.Home,
+                    painter = painterResource(id = R.drawable.skyexplorer_nav_icon_clear),
                     contentDescription = null,
-                    modifier = Modifier.size(40.dp)
+                    modifier = Modifier.size(100.dp),
+                    tint = Color.Unspecified
+
                 )
             }
         )
